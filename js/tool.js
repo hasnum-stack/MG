e8e7b67c9ee218f537aa6513d72399f148d94826
hasnum <hasnum@163.com>
2019-03-28 21:26:22 +0800

    cons

--- js/tool.js
+++ js/tool.js
@@ -1,1 +1,16 @@
-document.documentElement.style.fontSize = document.documentElement.clientWidth / 640 * 100 + 'px';
+//根据设计图设置对应的rem
+(function () {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 640 * 100 + 'px';
+}())
+
+(function () {
+  let con = `
+  <div class="logo">
+    <div class="back">
+      <img src="./images/icon_back.svg" alt="">
+    </div>
+  </div>
+  `
+  let header = document.querySelector('header');
+  header.innerHTML = con;
+}())
