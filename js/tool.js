d2a8d6a211895eb2dc7a050b7a5a4a23b05ad2dd
hasnum <hasnum@163.com>
2019-03-30 17:00:04 +0800

    conow

--- js/tool.js
+++ js/tool.js
@@ -1,16 +1,17 @@
 //根据设计图设置对应的rem
-(function () {
++function () {
   document.documentElement.style.fontSize = document.documentElement.clientWidth / 640 * 100 + 'px';
-}())
+}()
+
 //添加header
-(function () {
++function () {
   let con = `
   <div class="logo">
     <div class="back">
       <img src="./images/icon_back.svg" alt="">
     </div>
   </div>
-  `
+  `;
   let header = document.querySelector('header');
   header.innerHTML = con;
-}())
+}()
