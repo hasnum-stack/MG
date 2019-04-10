f6343b0677b8c34eafb07100300791b433b5e024
hasnum <hasnum@163.com>
2019-04-10 10:30:10 +0800

    all pages

--- js/tool.js
+++ js/tool.js
@@ -15,14 +15,14 @@ function () {
   </div>
   `;
   let header = document.querySelector('header');
-  header.innerHTML = con;
+  if (header) header.innerHTML = con;
 }()
 
 
 //预约讲座时间
 function cathedraTime() {
   let userChoose, itemName, teacherName, data, time;
-  $.each($('.cathedra_time_i span'), function (i, el) {
+  $.each($('.cathedra_time_i .isFull'), function (i, el) {
     let tpar = $(this).parent('.cathedra_time_i');
     if ($(el).html()) { //判断是否已满
       $(tpar).css('color', ' #979797');
@@ -30,10 +30,11 @@ function cathedraTime() {
       $(tpar).click(function () { //给未满元素添加点击
         $('.con').find('.cathedra_time_i').css('background-color', 'transparent');
         $(this).css('background-color', '#FF0000');
-        time = $(this).text();
+        time = $(this).children('p').text();
         data = $(this).parent('.cathedra_time').siblings('.cathedra_data').text();
-        teacherName = $(this).parents('.con').children('.teacher_name').text();
+        teacherName = $(this).children('.name').text();
         itemName = $(this).parents('.con').children('.cathedra_name').text();
+        console.log(time)
         userChoose = `
         您已选择:${itemName}-${teacherName} ${data} ${time}
         `
